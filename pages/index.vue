216a7b9e1c535d4b64a46b2c0a81cef888333bba
yuu-eguci <yuu.eguci@gmail.com>
2023-07-21 13:48:16 +0900

    ✨ 画像上に矩形を描いて、その部分の画像を切り抜く (ただし左上から右下への矩形選択のみ)

--- pages/index.vue
+++ pages/index.vue
@@ -44,8 +44,12 @@
           <canvas
             ref="overlayCanvas"
             style="position: absolute; top: 0; left: 0;"
+            @mousedown="startSelection"
+            @mousemove="updateSelection"
+            @mouseup="endSelection"
           />
         </div>
+        <img ref="croppedImage" alt="Cropped content">
       </div>
     </v-col>
   </v-row>
@@ -60,10 +64,58 @@ export default {
   data () {
     return {
       imgSrc: '',
-      changeVSheetColor: false
+      changeVSheetColor: false,
+      isSelecting: false,
+      selectionStart: { x: 0, y: 0 },
+      selectionEnd: { x: 0, y: 0 }
     }
   },
   methods: {
+    startSelection (event) {
+      // 矩形選択を開始した。
+      this.isSelecting = true
+      // 選択開始位置。
+      this.selectionStart = { x: event.offsetX, y: event.offsetY }
+    },
+
+    updateSelection (event) {
+      // 選択が始まっていない?
+      if (!this.isSelecting) {
+        return
+      }
+      // いまここ。
+      this.selectionEnd = { x: event.offsetX, y: event.offsetY }
+      // Canvas 取得。
+      const canvas = this.$refs.overlayCanvas
+      const ctx = canvas.getContext('2d')
+      // 前回の描画を消す。
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      // 選択範囲の矩形を描画。
+      const width = this.selectionEnd.x - this.selectionStart.x
+      const height = this.selectionEnd.y - this.selectionStart.y
+      ctx.strokeRect(this.selectionStart.x, this.selectionStart.y, width, height)
+    },
+
+    endSelection () {
+      // 選択終了。
+      this.isSelecting = false
+      // 選択範囲の幅と高さを計算。
+      const width = this.selectionEnd.x - this.selectionStart.x
+      const height = this.selectionEnd.y - this.selectionStart.y
+      // 選択範囲の画像を切り出す。
+      const canvas = document.createElement('canvas')
+      canvas.width = width
+      canvas.height = height
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(
+        this.$refs.uploadedImage,
+        this.selectionStart.x, this.selectionStart.y, width, height,
+        0, 0, width, height
+      )
+      // Canvas を data URL へ変換。
+      this.$refs.croppedImage.src = canvas.toDataURL()
+    },
+
     onDrop (event) {
       this.changeVSheetColor = false
       const files = event.dataTransfer.files
