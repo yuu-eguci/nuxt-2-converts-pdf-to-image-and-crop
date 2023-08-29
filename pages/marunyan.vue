a4c1f734fca0b71e247efa4d92ae634b9115d10f
yuu-eguci <yuu.eguci@gmail.com>
2023-08-29 17:06:25 +0900

    ⚗️ 画像を表示

--- pages/marunyan.vue
+++ pages/marunyan.vue
@@ -1,25 +1,72 @@
 <template>
   <v-row>
-    <v-col class="text-center">
+    <v-col
+      cols="8"
+      class="text-center"
+    >
       <img
-        src="/v.png"
+        src="/marunyan.png"
         alt="Vuetify.js"
         class="mb-5"
       >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+    </v-col>
+    <v-col
+      cols="4"
+      class="text-center"
+    >
+      <v-card
+        elevation="4"
+        class="mx-auto"
+      >
+        <v-text-field
+          v-model="mainText"
+          label="Selected text"
+          readonly
+        />
+      </v-card>
+      <v-card
+        elevation="4"
+        class="mx-auto"
+      >
+        <v-card-subtitle>
+          Activity log
+        </v-card-subtitle>
+        <v-divider />
+        <v-virtual-scroll
+          :items="activityLogs"
+          height="600"
+          item-height="56"
+        >
+          <template #default="{ item, index }">
+            <v-list-item :key="item.id">
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ index }} {{ item }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+          </template>
+        </v-virtual-scroll>
+      </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: 'InspirePage'
+  name: 'MarunyanPage',
+  data () {
+    return {
+      activityLogs: [],
+      mainText: ''
+    }
+  },
+  created () {
+    this.activityLogs.unshift('created が呼ばれたよ。')
+  },
+  mounted () {
+    this.activityLogs.unshift('mounted が呼ばれたよ。')
+  }
 }
 </script>
